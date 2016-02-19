b7cffe5ef558fd2010269cfc78cc2839042e1a84
Maurits <maurits.sanders@xs4all.nl>
2016-02-19 09:46:42 +0100

    Update to mjs_reboot.sh
    v1.3 from bless --device $DS_LAST_RESTORED_DEVICE to systemsetup -setstartupdisk "/Volumes/$DS_LAST_RESTORED_VOLUME"
    and deleted unneeded lines

--- mjs_reboot.sh
+++ mjs_reboot.sh
@@ -1,48 +1,20 @@
 #!/bin/sh
 
-echo "mjs_reboot.sh - v1.2 ("`date`")"
+echo "mjs_reboot.sh - v1.3 ("`date`")"
+#1.3: from bless device to -setstartupdisk volume
 
-#
-# DS_USER_LOGIN,
-# DS_REPOSITORY_PATH,
-# DS_LAST_RESTORED_VOLUME,
-# DS_LAST_RESTORED_DEVICE,
-# DS_LAST_IMAGED_VOLUME_FILE_NAME,
-# DS_BOOT_DEVICE,
-# DS_STARTUP_VOLUME,
-# DS_HOSTNAME,
-# DS_COMPUTERNAME,
-# DS_PRIMARY_MAC_ADDRESS,
-# DS_ASSIGNED_IP_ADDRESS,
-# DS_ASSIGNED_SUBNET,
-# DS_ASSIGNED_ROUTER,
-# DS_ASSIGNED_HOSTNAME,
-# DS_ASSIGNED_DNS,
-# DS_ASSIGNED_DOMAIN,
-# DS_SUPERDRIVE are environment variables defined by DeployStudio Server Runtime
-
-# echo 'DS_USER_LOGIN                  =('${DS_USER_LOGIN}')'
-# echo 'DS_REPOSITORY_PATH             =('${DS_REPOSITORY_PATH}')'
 echo 'DS_LAST_RESTORED_VOLUME        =('${DS_LAST_RESTORED_VOLUME}')'
 echo 'DS_LAST_RESTORED_DEVICE        =('${DS_LAST_RESTORED_DEVICE}')'
 echo 'DS_LAST_IMAGED_VOLUME_FILE_NAME=('${DS_LAST_IMAGED_VOLUME_FILE_NAME}')'
 echo 'DS_BOOT_DEVICE                 =('${DS_BOOT_DEVICE}')'
 echo 'DS_STARTUP_VOLUME              =('${DS_STARTUP_VOLUME}')'
-# echo 'DS_HOSTNAME                    =('${DS_HOSTNAME}')'
-# echo 'DS_COMPUTERNAME                =('${DS_COMPUTERNAME}')'
-# echo 'DS_PRIMARY_MAC_ADDRESS         =('${DS_PRIMARY_MAC_ADDRESS}')'
-# echo 'DS_ASSIGNED_IP_ADDRESS         =('${DS_ASSIGNED_IP_ADDRESS}')'
-# echo 'DS_ASSIGNED_SUBNET             =('${DS_ASSIGNED_SUBNET}')'
-# echo 'DS_ASSIGNED_ROUTER             =('${DS_ASSIGNED_ROUTER}')'
-# echo 'DS_ASSIGNED_HOSTNAME           =('${DS_ASSIGNED_HOSTNAME}')'
-# echo 'DS_ASSIGNED_DNS                =('${DS_ASSIGNED_DNS}')'
-# echo 'DS_ASSIGNED_DOMAIN             =('${DS_ASSIGNED_DOMAIN}')'
-# echo 'DS_SUPERDRIVE                  =('${DS_SUPERDRIVE}')'
 
 /bin/echo 'sleep 2'
 /bin/sleep 2
 
-/usr/sbin/bless --device $DS_LAST_RESTORED_DEVICE --setBoot
+echo 'Will systemset -setstartupdisk '$DS_LAST_RESTORED_VOLUME''
+/usr/sbin/systemsetup -setstartupdisk "/Volumes/$DS_LAST_RESTORED_VOLUME"
+#/usr/sbin/bless --mount "/Volumes/$DS_LAST_RESTORED_VOLUME" --setBoot
 /sbin/shutdown -r now
 
 #
